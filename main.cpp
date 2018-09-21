679baaa78de20527b9a90f51f5c9e0b7f8b94bfe
Jin-Suk Kim <99wlstjr@gmail.com>
2018-09-21 15:02:42 -0700

    reading text file and get values

--- main.cpp
+++ main.cpp
@@ -10,15 +10,45 @@ int main() {
     ofstream fout;
     int numList = (rand() % 513) + 512;
     double ranNum;
+    int lineNum;
+    double num;
+    double ave = 0;
+    double high = 0;
+    double low = 100;
+    double median;
+
     fout.open("Reading.txt");
     if(fout.is_open()){
         for(int i=0;i<numList;i++) {
             ranNum = (rand() /(double)RAND_MAX)* 40 + 50;
             fout << i << " " << fixed << setprecision(3) << ranNum << endl;
         }
-    }
-    if(fout.is_open()) {
         fout.close();
     }
+    ifstream fin;
+    fin.open("Reading.txt");
+    if(fin.is_opend()) {
+        istringstream strLine;
+        string line;
+
+        while (getline(fin, line)) {
+            strLine.clear();
+            strLine.str(line);
+            strLine >> lineNum;
+            strLine >> num;
+            ave += num;
+            if (num > high) {
+                high = num;
+            }
+            if (num < low) {
+                low = num;
+            }
+            if (lineNum == (numList / 2)) {
+                median = num;
+            }
+        }
+        fin.close();
+    }
+
     return 0;
 }

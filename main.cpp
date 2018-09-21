2279bbe99e31d322285512d4c7cfbaca8f514fc7
Jin-Suk Kim <99wlstjr@gmail.com>
2018-09-21 14:22:37 -0700

    random number list

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-using namespace std;
-int main() {
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <random>
 
-    
+using namespace std;
+
+int main() {
+    ofstream fout;
+    int numList = (rand() % 513) + 512;
+    double ranNum;
+    fout.open("Reading.txt");
+    if(fout.is_open()){
+        for(int i=0;i<numList;i++) {
+            ranNum = (rand() /(double)RAND_MAX)* 40 + 50;
+            fout << i << " " << fixed << setprecision(3) << ranNum << endl;
+        }
+    }
+    if(fout.is_open()) {
+        fout.close();
+    }
     return 0;
 }
